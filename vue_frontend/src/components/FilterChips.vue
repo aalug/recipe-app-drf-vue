09e8bc58d4b386da1335476e60b88cff063dc57e
Adam Gulczynski <adamg@example.com>
2023-03-12 15:41:36 +0100

    #34 completed. Filtering by tags and ingredients added.

---
+++ vue_frontend/src/components/FilterChips.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FilterChips"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue_frontend/src/components/loading/PageLoading.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PageLoading"
+}
+</script>
+
+<style scoped>
+
+</style>
